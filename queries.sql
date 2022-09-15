6fab431b59c927a8171a0743d8e90e79e1ed87bc
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2022-09-15 11:42:07 +0100

    Add: data.sql file, queries.sql file and schema.sql file

--- queries.sql
+++ queries.sql
@@ -1,3 +1,22 @@
-/*Queries that provide answers to the questions from all projects.*/
+/* Write query using JOIN */
 
-SELECT * from animals WHERE name = 'Luna';
+/*What animals belong to Melody Pond*/
+SELECT name FROM animals INNER JOIN owners ON animals.owners_id = owners.owners_id WHERE owners.full_name = 'Melody Pond';
+
+/*List of all animals that are pokemon*/
+SELECT animals.name FROM animals JOIN species ON animals.species_id = species.species_id WHERE species.name = 'Pokemon';
+
+/* List all owners and their animals, remember to include those that don't own any animal.*/
+SELECT owners.full_name, animals.name FROM animals RIGHT JOIN owners ON animals.owners_id = owners.owners_id ;
+
+/* How many animals are there per species?*/
+SELECT species.name, COUNT(*) FROM animals JOIN species ON animals.species_id = species.species_id GROUP by species.name;
+
+/*List all Digimon owned by Jennifer Orwell*/
+SELECT animals.name FROM animals JOIN owners ON animals.owners_id = owners.owners_id JOIN species ON animals.species_id = species.species_id WHERE owners.full_name = 'Jennifer Orwell' AND species.name = 'Digimon';
+
+/* List all animals owned by Dean Winchester that haven't tried to escape.*/
+SELECT animals.name FROM animals JOIN owners ON animals.owners_id = owners.owners_id WHERE owners.full_name = 'Dean Winchester' AND animals.escape_attempts = 0 GROUP BY animals.name;
+
+/*Who owns the most animals?*/
+SELECT owners.full_name, COUNT(*) FROM owners JOIN animals ON owners.owners_id = animals.owners_id GROUP BY owners.full_name ORDER BY COUNT(*) DESC LIMIT 1;
--- schema.sql
+++ schema.sql
@@ -1,5 +1,14 @@
-/* Database schema to keep the structure of entire database. */
-
-CREATE TABLE animals (
-    name varchar(100)
+CREATE TABLE owners (
+    owners_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    full_name VARCHAR(150) NOT NULL,
+    owners_age INT
 );
+
+CREATE TABLE species (
+    species_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name VARCHAR(150) NOT NULL
+);
+
+ALTER TABLE animals DROP COLUMN species;
+ALTER TABLE animals ADD COLUMN species_id INT REFERENCES species(species_id);
+ALTER TABLE animals ADD COLUMN owners_id INT REFERENCES owners(owners_id);
